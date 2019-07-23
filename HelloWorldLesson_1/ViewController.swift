6c34146b05f2f32e81eb919d2cb464256d273605
Roman Kobets <kobetsrg@gmail.com>
2019-07-23 20:35:41 +0300

    Initial Commit

---
+++ HelloWorldLesson_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldLesson_1
+//
+//  Created by Roman Kobets on 23/07/2019.
+//  Copyright © 2019 RomanKobets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
